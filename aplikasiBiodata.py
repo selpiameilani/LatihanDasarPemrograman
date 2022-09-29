3c30ef6332530a7a63ef0646ee7b1e9f1deb8802
selpiameilani <114548273+selpiameilani@users.noreply.github.com>
2022-09-28 21:28:35 -0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,22 @@
+# aplikasi biodata creat by selpia 
+
+# memasukan nama 
+nama = input ("masukan nama anda ?")
+#output nama 
+print("nama anda adalah: ", nama)
+#input kelas
+kelas = input("masukan kelas?")
+#output kelas 
+print("kelas anda adalah: ", kelas)
+#input nim 
+nim = input("masukan nim?")
+#output nim 
+print("nim anda adalah;:", nim)
+#input prodi
+prodi = input("masukan prodi?")
+#output prodi
+print("anda dari prodi:", prodi)
+#input tanggal lahir 
+tanggallahir = input("masukan tanggal lahir?")
+#output tanggal lahir
+print("taanggal lahir anda adalah: ", tanggallahir)
